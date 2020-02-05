5159e8f2c773fca18bd694d13a0b1899e48876b4
Ochieng424 <ochiengderrick424@gmail.com>
2020-02-05 20:54:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Laravel API Boilerplate using JWT
+
+A quick starting point to api development.
+
+### Build on top of:
+
+- [Laravel framework](https://laravel.com/)
+- [JWT-auth](https://jwt-auth.readthedocs.io/en/develop/)
+- [laravel-cors](https://github.com/fruitcake/laravel-cors)
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('v1')->group(function () {
+    Route::group(['middleware' => 'jwt.refresh'], function () {
+        Route::get('auth/refresh', 'API\AuthController@refresh');
+    });
+    Route::post('auth/register', 'API\AuthController@register');
+    Route::post('auth/login', 'API\AuthController@login');
+    Route::get('auth/user', 'API\AuthController@user');
+    Route::post('auth/logout', 'API\AuthController@logout');
+});
